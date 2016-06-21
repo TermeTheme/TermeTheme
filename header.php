5c7cf8e38597780f86f51efac9d52718b8548c3c
nimamamany007 <nima.rahimi@live.com>
2016-06-21 14:46:35 +0430

    Update Theme Option 1

--- header.php
+++ header.php
@@ -5,12 +5,12 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
 	<title>Home</title>
-	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<!-- <link rel="stylesheet" href="assets/css/bootstrap.min.css"> -->
 	<!-- <link rel="stylesheet" href="assets/css/bootstrap-theme.min.css"> -->
-	<link rel="stylesheet" href="assets/css/font-awesome.min.css">
-	<link rel="stylesheet" href="assets/css/owl.carousel.css">
-	<link rel="stylesheet" href="assets/css/slidebars.min.css">
-	<link rel="stylesheet" href="assets/css/terme.css">
+	<!-- <link rel="stylesheet" href="assets/css/font-awesome.min.css"> -->
+	<!-- <link rel="stylesheet" href="assets/css/owl.carousel.css"> -->
+	<!-- <link rel="stylesheet" href="assets/css/slidebars.min.css"> -->
+	<!-- <link rel="stylesheet" href="assets/css/terme.css"> -->
 	<?php if ($terme_options['custom_favicon']['url'] != '') { ?>
 	<link rel="shortcut icon" href="<?php echo $terme_options['custom_favicon']['url']; ?>" />
 	<?php } ?>
